fb95026c847c0de081dbfc91f1e45760406b6c9d
gloria866-crypto <essegloria781@gmail.com>
2026-08-02 23:07:18 +0000

    FCP-3:Create DynamoDB tables via Terraform

--- infra/main.tf
+++ infra/main.tf
@@ -10,3 +10,36 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+resource "aws_dynamodb_table" "events" {
+  name         = "Events"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "eventId"
+
+  attribute {
+    name = "eventId"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "registrations" {
+  name         = "Registrations"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "registrationId"
+
+  attribute {
+    name = "registrationId"
+    type = "S"
+  }
+
+  attribute {
+    name = "eventId"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "eventId-index"
+    hash_key        = "eventId"
+    projection_type = "ALL"
+  }
+}
